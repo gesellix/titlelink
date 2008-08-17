82f5e3309c683b4eff78d2a7343a2980d5eba840
Tobias Gesellchen <tobias@gesellix.de>
2008-08-17 22:22:56 +0000

    [#10182] SEF don't work correctly, method="upgrade"
    git-svn-id: http://joomlacode.org/svn/titlelink@6 de31ee7e-3f2c-0410-b67e-83f64df8b4ac

--- j_1_5_x/plugin/changelog.php
+++ j_1_5_x/plugin/changelog.php
@@ -1,4 +1,14 @@
 <?php
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die('Direct Access to this location is not allowed.');
+
+?>
+
+2008-08-18:
+
+- [#10182] SEF don't work correctly
+- method="upgrade" to enable users making updates without uninstall
+
+
+<?php
 ?>
