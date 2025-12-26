acd0eb10695f98d385e63b0cc43e8956fffd0dde
Tobias Gesellchen <tobias@gesellix.de>
2025-12-26 10:32:51 +0100

    Update changelog, add release artifact

--- plg_titlelink/changelog.php
+++ plg_titlelink/changelog.php
@@ -11,6 +11,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/) and this project adheres to [Semantic Versioning](https://semver.org/).
 
 ## [Unreleased]
+
+## [v4.0.1] - 2025-12-26
 ### Changed
 - Fixed various minor issues during Joomla 6 update check
 
